cb0aa5caeda77d36bdf25cff8c3565ec02dba77d
Juan Miqueo <jgil@MacBook-Air-de-Juan.local>
2024-01-07 03:03:22 +0100

    faraday connection + return errors

--- lib/api360degree/whatsapp.rb
+++ lib/api360degree/whatsapp.rb
@@ -15,7 +15,7 @@ module Api360degree
   #
   class WhatsApp
     attr_reader :api_key
-    
+
     # The endpoints
     API_CONTACT_ENDPOINT = 'https://waba.360dialog.io/v1/contacts'
     API_MESSAGES_ENDPOINT = 'https://waba.360dialog.io/v1/messages'
@@ -39,7 +39,7 @@ module Api360degree
     # to:: [String] Phone number
     # place_holders:: [Hash] of Arrays with place holders
     #
-    #    {header: ["Customer Name"], 
+    #    {header: ["Customer Name"],
     #     body: ["Order Number"]}
     #
     # template:: [String] template name
@@ -49,15 +49,15 @@ module Api360degree
     # == Returns::
     #
     # [Hash] the response body
-    # 
+    #
     # == Throws::
     #
     # [WhatsAppException] if error calling API
     #
-    def send_whatsapp(to, 
+    def send_whatsapp(to,
                       placeholders,
-                      template, 
-                      language, 
+                      template,
+                      language,
                       namespace)
 
       # = Check contact
@@ -84,9 +84,9 @@ module Api360degree
       # = Send the message
 
       # - Create Faraday instance
-      conn = Faraday.new(API_MESSAGES_ENDPOINT) do |faraday|
-               faraday.response :json 
-             end
+      conn = Faraday.new(API_MESSAGES_ENDPOINT)# do |faraday|
+              # faraday.response :json
+             #end
 
       # - Make a post the the connection
       response = conn.post do |req|
@@ -95,11 +95,11 @@ module Api360degree
             req.body = payload.to_json
       end
 
-      # - Check response
-      raise WhatsAppException.new("Error sending message to #{to}", 
-                                  response.body) unless response.status == 200 or response.status == 201
-      
-      response.body
+      # - Check response (400 is a error with a errors in hash)
+      raise WhatsAppException.new("Error sending message to #{to}",
+                                  response.body) unless response.status == 200 or response.status == 201 or response.status == 400
+
+      [(response.status == 200 or response.status == 201), JSON.parse(response.body)]
 
     end
 
@@ -130,10 +130,8 @@ module Api360degree
       # = Send the message
 
       # - Create Faraday instance
-      conn = Faraday.new(API_CONTACT_ENDPOINT) do |faraday|
-               faraday.response :json 
-             end
-      
+      conn = Faraday.new(API_CONTACT_ENDPOINT)
+
       # - Make a post the the connection
       response = conn.post do |req|
             req.headers['Content-Type'] = 'application/json'
@@ -142,7 +140,7 @@ module Api360degree
       end
 
       # - Process response
-      raise WhatsAppException.new("Error checking contact #{contact}", 
+      raise WhatsAppException.new("Error checking contact #{contact}",
                                   response.body) unless response.status == 200 or response.status == 201
 
       # Return the response body
@@ -159,7 +157,7 @@ module Api360degree
     #
     # == Example
     #
-    #    {header: ["Customer Name"], 
+    #    {header: ["Customer Name"],
     #     body: ["Order Number"]}
     #
     # == Returns::
@@ -175,34 +173,34 @@ module Api360degree
       if placeholders.has_key?(:header) and placeholders[:header].is_a?(Array)
         components << {
                         "type" => "header",
-                        "parameters" => placeholders[:header].map { |placeholder| {type: 'text', text: placeholder.to_s} } 
+                        "parameters" => placeholders[:header].map { |placeholder| {type: 'text', text: placeholder.to_s} }
                       }
       end
       # - Body parameters
       if placeholders.has_key?(:body) and placeholders[:body].is_a?(Array)
         components << {
                         "type" => "body",
-                        "parameters" => placeholders[:body].map { |placeholder| {type: 'text', text: placeholder.to_s} } 
+                        "parameters" => placeholders[:body].map { |placeholder| {type: 'text', text: placeholder.to_s} }
                       }
       end
       # - Footer parameters
       if placeholders.has_key?(:footer) and placeholders[:footer].is_a?(Array)
-        components << { 
+        components << {
                         "type" => "footer",
-                        "parameters" => placeholders[:footer].map { |placeholder| {type: 'text', text: placeholder.to_s} } 
+                        "parameters" => placeholders[:footer].map { |placeholder| {type: 'text', text: placeholder.to_s} }
                       }
-      end   
+      end
 
       # - Buttons
       if placeholders.has_key?(:buttons) and placeholders[:buttons].is_a?(Hash)
         placeholders[:buttons].each do |key, value|
           next unless value.has_key?(:sub_type) and ['url','quick_reply'].include?(value[:sub_type])
           next unless value.has_key?(:parameters) and value[:parameters].is_a?(Array)
-          components << { 
+          components << {
                           "type" => "button",
                           "sub_type" => value[:sub_type],
                           "index" => key,
-                          "parameters" => value[:parameters].map { |placeholder| {type: 'text', text: placeholder.to_s} } 
+                          "parameters" => value[:parameters].map { |placeholder| {type: 'text', text: placeholder.to_s} }
                         }
         end
       end
